11bdb0b6fbd8fae651db38b522319e1c02dbfaf1
laszlobalo <laszlo.balo@edu.fh-joanneum.at>
2016-11-04 16:43:30 +0100

    Commit

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -10,17 +10,72 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-		//TODO: main() is missing
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.doIt();
+    }
+
+    private void doIt() {
+
+        List<GeometricShape> shapeList = new ArrayList<>();
+
+        fillList(shapeList);
+
+        printList(shapeList);
+
+        List<Circle> circlesList = new ArrayList<>();
+        for (GeometricShape shape : shapeList) {
+            if (shape instanceof Circle) {
+                circlesList.add((Circle)shape);
+            }
+
+        }
+        System.out.println("-----circles-------");
+        for (Circle circle : circlesList) {
+            System.out.println(circle);
+        }
+
+    }
+
+
 
 
     public void fillList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        shapeList.add(new Circle(1,2,2.0));
+        shapeList.add(new Circle(1,3,6.0));
+        shapeList.add(new Circle(1,4,5.0));
+
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+
+        shapeList.add(new Rectangle(1,2,3.0,21.0));
 
     }
 
 
     public void printList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        System.out.println("-----for loop-----");
+        for (int pos = 0; pos < shapeList.size(); pos++) {
+            System.out.println(shapeList.get(pos));
+        }
+
+        System.out.println("-----for each------");
+        for (GeometricShape geometricShape : shapeList) {
+            System.out.println(geometricShape);
+        }
+        System.out.println("-----stream1------");
+
+        shapeList
+                .stream()
+                .forEach(shape-> System.out.println(shape));
+
+        System.out.println("-----stream2------");
+        shapeList
+                .stream()
+                .forEach(System.out::print);
 
     }
 
